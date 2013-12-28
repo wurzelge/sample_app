ba2123b8f5924e774d48b6cd34c01642a9bc5b63
wurzel <wurzelge@thekeirs.com>
2013-12-28 12:56:45 -0800

    Improve tests per chapter exercises

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ SampleApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # Force SSL in production for security 
+  config.force_ssl = true
 end
