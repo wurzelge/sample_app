21de29633c6982f023138a189fa03c131cb882dc
wurzel <wurzelge@thekeirs.com>
2013-12-28 23:02:19 -0800

    Added user index

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -28,6 +28,7 @@ describe "Authentication" do
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Users',       href: users_path)   }
       it { should have_link('Sign out',    href: signout_path) }
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should_not have_link('Sign in', href: signin_path) }
@@ -71,6 +72,10 @@ describe "Authentication" do
             expect(page).to have_title('Edit user')
           end
         end
+      end
+      describe "visiting the user index" do
+        before { visit users_path }
+        it { should have_title('Sign in') }
       end
     end
 
